10a338fc477ff4d670841100c4a4f584fcb91247
yoon92kr <yoon92_kr@naver.com>
2021-10-18 17:46:53 +0900

    2021.10.18 VO ▶ DBMS의 쿼리문 시행결과를 담을 객체, 실질적인 데이터 보관 ▶ 사용하고자 하는 데이터베이스의 필드명과 동일한 변수를 가지는것이 일반적이며, getter / setter로 이루어져있다 ▶ 필요에 의해 기본생성자 외 파라미터 타입 별 생성자를 선언하기도 한다

---
+++ Code005.java
@@ -0,0 +1,44 @@
+package JustRunJSP;
+
+import java.sql.Date;
+
+public class Code005 {
+
+	private String name;
+	private String id;
+	private String email;
+	private String pwd;
+	private Date joinDate;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	
+}
