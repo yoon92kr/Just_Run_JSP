b4b5c9d67ac2ad7c43867ee5c1cc0be0f79afc68
yoon92kr <yoon92_kr@naver.com>
2021-10-19 15:55:37 +0900

    2021.10.19 ServletContext ▶ 컨테이너별 하나씩 존재하는 Context로, 컨테이너 실행시 생성 / 종료시 소멸 ▶ 데이터 바인딩 / 로그파일 / 설정정보 제공 기능 ▶ 지원 메소드 getAttribute(String name) : 주어진 name에 해당하는 바인딩 된 value를 반환, 존재하지 않으면 null getAttributeNames() : 바인딩된 속성들의 name(key)를 반환 getContext(String uriPath) : 지정한 uriPath에 해당되는 객체 반환 setAttribute(String name, Object object) : 해당 name(key)으로 object 객체를 ServletContext에 바인딩

---
+++ Code008.java
@@ -0,0 +1,46 @@
+package JustRunJSP;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Code008")
+public class Code008 extends HttpServlet {
+	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		ServletContext context = getServletContext();
+		// ServletContext의 객체를 가져오는 메소드
+		
+		List member = new ArrayList();
+		member.add("param1");
+		member.add("param2");
+		context.setAttribute("member", member);
+		// servletContext 객체에 key:member / value:memeber을 바인딩
+		
+
+	}
+
+}
