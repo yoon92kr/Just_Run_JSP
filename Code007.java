ba551ccf1db594379a075cb68ed65a37b27b4f14
yoon92kr <yoon92_kr@naver.com>
2021-10-19 10:00:50 +0900

    2021.10.19 Dispatch ▶ 웹클라이언트를 거치지 않고, 페이지에서 페이지로 포워딩을 진행한다. ▶ 제어 권한을 넘기기 전 출력버퍼를 비우기 때문에, 첫번째 페이지에서 출력버퍼를 선언하였더라도, 두번쩨 페이지의 내용만 웹 클라이언트에게 전달된다.

---
+++ Code007.java
@@ -0,0 +1,38 @@
+package JustRunJSP;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Code007")
+public class Code007 extends HttpServlet {
+	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher("dispatch servlet");
+		 
+		dispatch.forward(request, response);
+
+	}
+
+}
